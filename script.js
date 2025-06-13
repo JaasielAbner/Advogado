569efb2967f7542426ec3258030640b75ba5d21b
Jaasiel Abner <jabner101@gmail.com>
2025-06-13 15:57:33 -0300

    add microsoft analytics

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("[data-track]").forEach(el => {
         el.addEventListener("click", () => {
             console.log("Tracked:", el.getAttribute("data-track"));
-            // You can enhance this with real analytics tracking if needed.
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ryzgc4ng9o");
         });
     });
 });
