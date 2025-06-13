6e42f38ffd492dfc2448410f66beb22136e2d711
jaasielabner <jabner101@gmail.com>
2025-06-13 17:16:52 -0300

    fix analytics

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("[data-track]").forEach(el => {
         el.addEventListener("click", () => {
             console.log("Tracked:", el.getAttribute("data-track"));
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "ryzgc4ng9o");
         });
     });
 });
